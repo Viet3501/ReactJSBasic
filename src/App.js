335a9e71146fbf3adb6600e7d2cdbb4072ae9900
Viet3501 <nguyentruongviet2001@gmail.com>
2022-12-06 10:09:36 +0700

    video2

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ const App = () => {
     // JSX
     // babel
     // SPA : single page aplication
+
+    let name = 'Việt';
+    let number = 2022;
+    let boolean = true;
+    let obj = { name: 'Việt', age: 21 };
     return (
         <div className="App">
+            {console.log(obj)}
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>My name is Việt</p>
+                <span>
+                    My name is {name} in {number}
+                </span>
+                <p>{JSON.stringify(obj)}</p>
+                <a href="" target = '_blank'>Visit my channel</a>
             </header>
         </div>
     );
